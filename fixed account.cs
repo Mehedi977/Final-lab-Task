cc0d6a1cacf4ec4f02762acc8cfe88e6af113d4e
MehediHasan <nayan_hasan20@yahoo.com>
2020-11-23 21:58:44 +0600

    final lab task 1

---
+++ fixed account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalLab task 
+{
+    class FixedAccount : Account
+    {
+        private  int TenureYear { get; set; }
+        public FixedAccount(string name, string id, double balance, int year) : base(name, id, balance)
+        {
+            TenureYear = year;
+        }
+        public override void Withdraw(double amount)
+        {
+            if (DateTime.Now.Year > TenureYear)
+            {
+                if (Balance - amount >= 0)
+                {
+                    Balance += amount;
+                }
+                else
+                {
+                    Console.WriteLine("Cannot Withdraw..... Balance will be less than zero.Current Balance is :    " + Balance);
+                
+            }
+            else
+            {
+                Console.WriteLine("Cannot Withdraw.....Account Did not reach Tenure Year. account Tenure Year :  " + TenureYear);
+            }
+        }
+    }
+}
---
+++ saving account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalLab task 
+{
+    class SavingAccount 
+    {
+        public SavingAccount(string name, string id, double balance) : base(name, id, balance)
+        {
+
+        }
+        public override void Withdraw(double amount)
+        {
+            if ((Balance - amount) < 500) 
+
+            Console.WriteLine("Cannot withdraw below 500 taka.Current Balance:  " + Balance);
+
+            else Balance += amount;
+
+        }
+    }
+}
