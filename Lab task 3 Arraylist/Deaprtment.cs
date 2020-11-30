028a2db6f75f78f071727d01aaccfbe139025414
MehediHasan <nayan_hasan20@yahoo.com>
2020-11-30 15:01:32 +0600

    lab task3

---
+++ Lab task 3 Arraylist/Deaprtment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_LabTask
+{
+    class  faculty
+    {
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+        public faculty() { }
+        public faculty(string Name, string Id,)
+        {
+            this.Name = Name;
+            this.Id = Id;
+
+        }
+        public void Show()
+        {
+            Console.WriteLine("Name: " + Name);
+
+            Console.WriteLine("Id: " + Id);
+        }
+    }
+}
---
+++ Lab task 3 Arraylist/Faculty.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Lab_Task3
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Faculty f = new Faculty("Tanvir ahmed.", "324532");
+
+            Faculty fac1 = new Faculty("Dr.Asraf ali.,345432");
+
+            Department dept = new Dept("Coe", "2");
+
+            dept.AddFaculty(f, fac1);
+
+            dept.ShowAll();
+        }
+    }
+}
